2b4964872006224adcd095a56d4914e306dfd20a
dlantjdgkgk <dlantjdgkgk@naver.com>
2021-11-19 17:23:12 +0900

    문제

--- JavaScript/K번째수.js
+++ JavaScript/K번째수.js
@@ -16,3 +16,22 @@ let commands = [
     [1, 7, 3],
 ];
 console.log(solution(array, commands));
+
+// Array.prototype.slice()  arr.slice([begin[,end]])
+// slice() 메서드는 어떤 배열의 begin부터 end까지(end 미포함)에 대한 얕은 복사본을 새로운 배열 객체로 반환합니다.
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+console.log(animals.slice(2));
+// expected output: Array ["camel", "duck", "elephant"]
+
+console.log(animals.slice(2, 4));
+// expected output: Array ["camel", "duck"]
+
+console.log(animals.slice(1, 5));
+// expected output: Array ["bison", "camel", "duck", "elephant"]
+
+console.log(animals.slice(-2));
+// expected output: Array ["duck", "elephant"]
+
+console.log(animals.slice(2, -1));
+// expected output: Array ["camel", "duck"]
